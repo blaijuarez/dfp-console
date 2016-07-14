fd4ee5b413bb3b7e629116b281cfddafa33df3c9
Blai <blaijuarez@gmail.com>
2016-07-14 13:14:52 +0200

    Muestra la consola tras refrescar publicidad. (#10)

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,16 +6,11 @@
 
             var redirect = {};
 
-            /*if (/^.+(tag\/js\/gpt)\.js$/.test(response.url) && !oneTime) {
-                //redirect.redirectUrl =  chrome.extension.getURL('scripts/gpt.js');
-                //redirect.cancel =  true;
-            }*/
-
             if (/^.+(gpt\/pubads_impl.+)\.js$/.test(response.url)) {
                 redirect.redirectUrl =  chrome.extension.getURL('scripts/pubads_impl_90.js');
             }
             return redirect;
         },
-        {urls: ["*://*.googleadservices.com/*","*://*.googletagservices.com/*","*://*/*"], types: ["script"]},
+        {urls: ["*://*.googleadservices.com/*"], types: ["script"]},
         ["blocking"]);
 }());
