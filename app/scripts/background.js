006f13b17404e160fe9d1ddc53b50cea57d84635
Blai Juarez <blai.juarez@colaboradoresunidadeditorial.es>
2016-08-10 14:22:29 +0200

    Añadido opción reporte.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,15 +1,31 @@
 (function() {
-    "use_strict";
+    "use strict";
+
+    var millisecondsPerCentury = 1000 * 60 * 60 * 24 * 7 * 30 * 12 * 100;
+    var oneCenturyAgo = (new Date()).getTime() - millisecondsPerCentury;
+
+    chrome.runtime.onMessage.addListener(
+        function(request) {
+            if (request.action == "removeUserBrowserData") {
+                chrome.browsingData.remove({
+                    "since": oneCenturyAgo,
+                    "originTypes": {
+                        "unprotectedWeb": true
+                    }
+                }, {
+                    "cache": true,
+                    "cookies": true
+                }, null);
+            }
+        });
 
     chrome.webRequest.onBeforeRequest.addListener(
         function (response) {
-
-            var redirect = {};
-
             if (/^.+(gpt\/pubads_impl.+)\.js$/.test(response.url)) {
+                var redirect = {};
                 redirect.redirectUrl =  chrome.extension.getURL('scripts/pubads_impl_90.js');
+                return redirect;
             }
-            return redirect;
         },
         {urls: ["*://*.googleadservices.com/*"], types: ["script"]},
         ["blocking"]);
