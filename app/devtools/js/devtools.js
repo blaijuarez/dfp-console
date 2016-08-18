6ac65a8901968cb5647bcc42526725dba48006ac
Blai <blaijuarez@gmail.com>
2016-08-18 12:27:32 +0200

    Develop (#17)
    * Ajuste versión v0.3.0
    
    * Solventado problema al crear la bbdd por primera vez.
    
    * Feature/devtool (#16)
    
    * Añadida comunicación con panel devtool.
    
    * Añadida interface de datos para la devtool

---
+++ app/devtools/js/devtools.js
@@ -0,0 +1,8 @@
+(function() {
+    "use strict";
+
+    chrome.devtools.panels.create("DFP Console",
+        null,
+        "devtools/panel.html",
+        null);
+}());
--- app/scripts/background.js
+++ app/scripts/background.js
@@ -19,6 +19,9 @@
             }
         });
 
+
+//////////////////////////////////////////////////////////
+
     chrome.webRequest.onBeforeRequest.addListener(
         function (response) {
             if (/^.+(gpt\/pubads_impl.+)\.js$/.test(response.url)) {
@@ -29,4 +32,53 @@
         },
         {urls: ["*://*.googleadservices.com/*"], types: ["script"]},
         ["blocking"]);
+
+
+//////////////////////////////////////////////////////////
+
+    var connections = {};
+
+    chrome.runtime.onConnect.addListener(function (port) {
+        function extensionListener(message, sender, sendResponse) {
+
+            if (message.from && message.from == "init-dfp") {
+                connections[message.tabId] = port;
+            }
+
+            if (message.from && message.from == "devtools") {
+                chrome.tabs.query({
+                    active: true,
+                    currentWindow: true
+                }, function (tabs) {
+                    for (var tab in tabs) {
+                        chrome.tabs.sendMessage(tabs[tab].id, message);
+                    }
+                });
+            }
+        }
+
+        port.onMessage.addListener(extensionListener);
+
+        port.onDisconnect.addListener(function(port) {
+            port.onMessage.removeListener(extensionListener);
+
+            var tabs = Object.keys(connections);
+            for (var i=0, len=tabs.length; i < len; i++) {
+                if (connections[tabs[i]] == port) {
+                    delete connections[tabs[i]];
+                    break;
+                }
+            }
+        });
+    });
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (sender.tab) {
+            var tabId = sender.tab.id;
+            if (tabId in connections) {
+                connections[tabId].postMessage(request);
+            }
+        }
+        return true;
+    });
 }());
